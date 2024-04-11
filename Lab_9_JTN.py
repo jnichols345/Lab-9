7f6af8d42b07a80aefd3f8f3b7770e500a82c01b
jnichols345 <jnichols2@ufl.edu>
2024-04-10 21:37:19 -0400

    Heres lab 9

--- Lab_9_JTN.py
+++ Lab_9_JTN.py
@@ -1,7 +1,15 @@
 #Jackson Nichols
 
 def encode_password(password):
+    new_pw = []
+
     for char in password:
+        val = ord(char) + 3
+        new_pw.append(chr(val))
+    final_pw = ''.join(new_pw)
+    return final_pw
+
+
 
 
 
